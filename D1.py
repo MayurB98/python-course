309d313190c4882a34f6218aaeb0eef23332f377
MayurB98 <121539848+MayurB98@users.noreply.github.com>
2023-02-21 12:41:10 +0530

    Add files via upload

---
+++ D1.py
@@ -0,0 +1,14 @@
+eid,ename,esal = 111,"Mona",10000.45
+print(eid)
+print(ename)
+print(esal)
+print("Emp id =",eid)
+print("Emp name =",ename)
+print("Emp sal=",esal)
+print(eid,ename,esal)
+print("Empid = ",eid,"Emp name = ",ename,"Empsal = ",esal)
+
+print("Emp id = %d Emp name = %s Emp sal = %g" %(eid,ename,esal))
+print("Emp id:{} Emp name:{} Emp sal:{}".format(eid,ename,esal))
+print("Emp id:{0} Emp name: {1}Emp sal{2}".format(eid,ename,esal))
+print("Emp id ={0}\n Emp name = {1}\n Empsal = {2}".format(eid,ename,esal))
