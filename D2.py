d9cf63de762001e1bfe7789b0e3b6cf5566c4c18
MayurB98 <121539848+MayurB98@users.noreply.github.com>
2023-02-22 13:01:49 +0530

    Add files via upload

---
+++ D2.py
@@ -0,0 +1,35 @@
+"""decimal =10
+print(bin(decimal))
+print(oct(decimal))
+print(hex(decimal))
+
+a,b,c,d= 0B10,0O10,10,0X10
+print(a,b,c,d)
+
+hexa = 0x2b
+print(hexa)
+oct=0o0745
+print(oct)
+bin=0b1100110
+print(bin)"""
+
+"""c = input("Enter a character : ")
+print("The Ascii value of"" +c ""is",ord(c))
+
+print(chr(0xaa))        #oUTPUT - a
+print(chr(0xff))        #output - ÿ
+print(chr(200))         #output - E
+print(chr(122))         #OUTPUT """
+
+print(int(123.987))
+print(int(True))
+print(int(False))
+print(float(False))
+print(str(False))
+
+a,b = 10,20
+print*(str(a) + str(b))
+print(int("10"))
+print(float("10"))
+print(int("10.5"))
+print(int("Hello"))         # error insted of int use str
