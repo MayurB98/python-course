68d189b2b2844f3eb5c10ae43dfe83127f13bfbb
MayurB98 <121539848+MayurB98@users.noreply.github.com>
2023-02-22 13:01:25 +0530

    Add files via upload

---
+++ D3_IF.py
@@ -0,0 +1,33 @@
+"""age = int(input("Enter your age"))
+if (age>=18):
+        print("vote")
+else:
+        print("Don't vote") """
+
+"""n1 =int(input("Enter first number"))
+n2 = int(input("Enter second number"))
+if(n1>n2):
+        print("N1 is greater thsn n2")
+else:
+        print("n2 is greater") """
+
+"""if 0:
+        print("hi amit")
+else:
+        print("hi anu")"""
+
+r1=float(input("Enter radius 1"))
+r2=float(input("Enter radius 2"))
+
+
+area1 = 3.14*r1*r1
+area2 = 3.14*r2*r2
+
+if(area1>area2):
+        diff = area1-area2
+        print("Area1 is greater and difference is " ,diff)
+else:
+        diff = area2-area1
+        print("Area 2 is greater and difference is ",diff)
+    
+
