385a50ed769f66ccf9eb5d7d450edb42b666bf7f
MayurB98 <121539848+MayurB98@users.noreply.github.com>
2023-02-23 12:45:29 +0530

    Add files via upload

---
+++ D4.py
@@ -0,0 +1,59 @@
+"""a=int(input("Enter choice :"))
+if a==10:
+        print("pratap")
+elif a==20:
+        print("Anu")
+elif a==30:
+        print("Durga")
+else:
+        print("Kavya") """
+
+"""x=int(input("Please enter an integer :"))
+if x<0:
+        print("x is negative value")
+elif x%2==0:
+        print("Your values are positive and even")
+else:
+        print("Your values are positive and odd")"""
+
+# to check if number is positive
+
+"""num=int(input("Enter your number"))
+if(num>0):
+        print("Number is positive")
+else:
+        print("Number is negative" )"""
+
+# pgm to check multiple of 5
+
+"""num=int(input("enter your number"))
+if(num%5==0):
+        print("Number is divisible by  5")
+else:
+        print("Number is not divisble by 5")"""
+
+# pgm to check leap year
+
+"""year=int(input("Enter year"))
+if year%4==0:
+        print("It  is leap year")
+else:
+        print("It is not leap year")"""
+        
+
+#pgm to give discount of 10%
+bill = int(input("Enter bill amount"))
+if bill>1000:
+        disc=bill*0.10
+        print("disc = ",bill-disc)
+else:
+        print("No discount")
+
+        
+
+    
+
+        
+
+        
+
